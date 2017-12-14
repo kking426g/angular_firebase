3cd58200c8846a5d5e45757ab93f9063e3318946
Joanne Wu <joannewu@astrocorp.com.tw>
2017-12-14 17:55:25 +0800

    learning from https://www.youtube.com/watch?v=gUmItHaVL2w

---
+++ angularfs/src/app/components/items/items.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
+import { Item } from '../../models/Item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+  items: Item[];
+
+  constructor(private itemService: ItemService) { }//only for use when u want to inject something like service
+
+  //initalization do in here ex: fetching stuff
+  ngOnInit() {//lifecycle run autometiclly when the component is generate    we want to fetch item thourgh the service
+    console.log('ngOninit ran')
+    this.itemService.getItems().subscribe(items => {
+      console.log(items)
+      this.items = items;
+    });
+  }
+
+}
---
+++ angularfs/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD1mNv9AcW5NqhEwd2MCpZCPDBDis3lB8Y",
+    authDomain: "fs1prod-16bed.firebaseapp.com",
+    databaseURL: "https://fs1prod-16bed.firebaseio.com",
+    projectId: "fs1prod-16bed",
+    storageBucket: "fs1prod-16bed.appspot.com",
+    messagingSenderId: "45770827147"
+  }
+};
