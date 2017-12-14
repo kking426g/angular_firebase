e1a1ac88a533f8ce73e222b765a02b2b50df5657
kking426g <kking426g@gmail.com>
2017-12-14 23:47:02 +0800

    learning from https://www.youtube.com/watch?v=cwqeyOFcaoA&t=8s

--- angularfs/src/app/services/item.service.ts
+++ angularfs/src/app/services/item.service.ts
@@ -7,14 +7,35 @@ import { Observable } from 'rxjs/Observable';
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
+  itemDoc: AngularFirestoreDocument<Item>;
 
   constructor(public afs: AngularFirestore) { 
-    this.items = this.afs.collection('items').valueChanges();//datastring
+    //this.items = this.afs.collection('items').valueChanges();//datastring
+
+    this.itemsCollection = this.afs.collection('items', ref => ref.orderBy('title', 'asc'));
+
+    this.items = this.itemsCollection.snapshotChanges().map(changes =>{//sometimes is action, somtimes is changes
+      return changes.map( a =>{
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    });
   }
 
   getItems(){
     return this.items;
   }
-  
+
+  addItem(item: Item){
+    this.itemsCollection.add(item);
+  }
+
+  deleteItem(item: Item){
+    this.itemDoc = this.afs.doc(`items/${item.id}`);
+    //console.log(this.itemDoc)
+    this.itemDoc.delete();
+  }
+
 }
 
